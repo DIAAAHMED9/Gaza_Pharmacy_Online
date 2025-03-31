9afaf85fbf8808015c58917bf89cd9eeac42753c
Diaa Rahma <97956359+DIAAAHMED9@users.noreply.github.com>
2025-03-31 21:10:01 +0200

    Update cart.dart

--- lib/controller/cart.dart
+++ lib/controller/cart.dart
@@ -96,6 +96,7 @@ class CartController extends GetxController {
         }
       }
       return cartProducts;
-    });
+    }
+                                                             );
   }
 }
